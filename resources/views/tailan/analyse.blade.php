b5b599e675f3938db4c0a5c11bfab45532e61ee2
selengetu <selengetu@gmail.com>
2019-05-07 08:14:05 +0800

    5.7 done

--- resources/views/tailan/analyse.blade.php
+++ resources/views/tailan/analyse.blade.php
@@ -291,7 +291,7 @@
                 options: {
                     title: {
                         display: true,
-                        text: 'Predicted world population (millions) in 2050'
+
                     }
                 }
             });
