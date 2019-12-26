6040f76ceb2cbcc3cfb0007981401f8f79ef135e
selengetu <selengetu@gmail.com>
2019-12-27 07:57:37 +0800

    duus

--- resources/views/tailan/main.blade.php
+++ resources/views/tailan/main.blade.php
@@ -387,7 +387,7 @@
                                                     @endif
                                                     color="white"
                                                 >    <font  @if($projects->state_id==1)
-                                                            color="black"; @else color="white"; @endif >{{$projects->state_name_mn}}<br>{{$projects->state_name_ru}}</font></td></td>
+                                                            color="black"; @else color="white"; @endif >{{$projects->state_name_mn}}<br>{{$projects->state_name_ru}}<br>{{$projects->description}}</font></td></td>
 
                                                 <td>@if($projects->image_b1!=null)<img src="<?php echo asset("profile_images/img/$projects->image_b1")?>"  height="100" width="100" onclick="preview_image({{$projects->project_id}})" data-toggle="modal" data-target="#photomodal">@endif</td>
                                             </tr>
