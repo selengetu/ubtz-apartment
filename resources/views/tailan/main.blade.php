4d21ea8e64fe052d7669e69e163ac695377890c1
selengetu <49024084+selengetu@users.noreply.github.com>
2020-12-15 15:45:31 +0800

    font

--- resources/views/tailan/main.blade.php
+++ resources/views/tailan/main.blade.php
@@ -343,7 +343,7 @@
                                                     bgcolor="red";
                                                     @endif
                                                     color="white"
-                                                >    <font  @if($projects->state_id==1)
+                                                >    <font  @if($projects->state_id==1 or $projects->state_id==4)
                                                             color="black"; @else color="white"; @endif >{{$projects->state_name_mn}}<br>{{$projects->state_name_ru}}<br>{{$projects->description}}</font></td>
                                                 <td>@if($projects->image_b1!=null)<img src="<?php echo asset("profile_images/img/$projects->image_b1")?>"  height="100" width="100" onclick="preview_image({{$projects->project_id}})" data-toggle="modal" data-target="#photomodal">@endif</td>
 
@@ -435,7 +435,7 @@
                                                     bgcolor="red";
                                                     @endif
                                                     color="white"
-                                                >    <font  @if($projects->state_id==1)
+                                                >    <font  @if($projects->state_id==1 or $projects->state_id==4)
                                                             color="black"; @else color="white"; @endif >{{$projects->state_name_mn}}<br>{{$projects->state_name_ru}}<br>{{$projects->description}}</font></td>
 
                                                 <td>@if($projects->image_b1!=null)<img src="<?php echo asset("profile_images/img/$projects->image_b1")?>"  height="100" width="100" onclick="preview_image({{$projects->project_id}})" data-toggle="modal" data-target="#photomodal">@endif</td>
@@ -671,7 +671,7 @@
                                                     bgcolor="red";
                                                     @endif
                                                     color="white"
-                                                >    <font  @if($projects->state_id==1)
+                                                >    <font  @if($projects->state_id==1 or $projects->state_id==4)
                                                             color="black"; @else color="white"; @endif >{{$projects->state_name_mn}}<br>{{$projects->state_name_ru}}<br>{{$projects->description}}</font></td></td>
                                                 </tr>
                                             <?php $no++; ?>
@@ -750,7 +750,7 @@
                                                     bgcolor="red";
                                                     @endif
                                                     color="white"
-                                                >    <font  @if($projects->state_id==1)
+                                                >    <font  @if($projects->state_id==1 or $projects->state_id==4)
                                                             color="black"; @else color="white"; @endif >{{$projects->state_name_mn}}<br>{{$projects->state_name_ru}}<br>{{$projects->description}}</font></td></td>
 
                                                  </tr>
