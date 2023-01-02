d00c1b90105f90e81061ed96944d90ae8427e02d
runism <selengetu@gmail.com>
2023-01-02 18:40:34 +0800

    2023

--- app/Http/Controllers/BarilgaController.php
+++ app/Http/Controllers/BarilgaController.php
@@ -206,8 +206,8 @@ class BarilgaController extends Controller
         }
         else
         {
-            $syear_id=2022;
-            $query.="and plan_year = 2022";
+            $syear_id=2023;
+            $query.="and plan_year = 2023";
 
         }
         if ($schildabbr!=NULL && $schildabbr !=0) {
@@ -313,7 +313,7 @@ class BarilgaController extends Controller
             $sprojecttype=2;
         }
         $project = new Project;
-        $project->plan_year = 2022;
+        $project->plan_year = 2023;
         $project->project_name = Request::input('project_name');
         $project->project_name_ru = Request::input('project_name_ru');
         $project->budget =preg_replace('/[a-zZ-a,]/', '',Request::input('budget'));
